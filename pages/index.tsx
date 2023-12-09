70e7b98627a3c5ed7af144cd3204a99576485357
Ivoine <ivoinestrachan242@gmail.com>
2023-12-09 10:29:06 -0500

    markdown

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
-import React, { useEffect, useState, useRef } from 'react';
-import Prism from 'prismjs';
-import 'prismjs/themes/prism-tomorrow.css';
-import Navbar from '../components/NavBar/Navbar';
+import React, { useEffect, useState, useRef } from "react";
+import Navbar from "../components/NavBar/Navbar";
 
 export default function Home() {
-  const [code, setCode] = useState('');
+  const [code, setCode] = useState("");
   const codeDisplayRef = useRef<HTMLPreElement>(null);
 
   useEffect(() => {
-    Prism.highlightAll();
+    if (codeDisplayRef.current) {
+      codeDisplayRef.current.innerHTML = convertMarkdownToHtml(code);
+    }
   }, [code]);
 
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -16,32 +16,55 @@ export default function Home() {
   };
 
   const getLineNumbers = () => {
-    const numberOfLines = code.split('\n').length;
-    return Array.from({ length: numberOfLines }, (_, i) => i + 1).join('\n');
+    const numberOfLines = code.split("\n").length;
+    return Array.from({ length: numberOfLines }, (_, i) => i + 1).join("\n");
   };
 
   const handleScroll = (e: React.UIEvent<HTMLTextAreaElement>) => {
-    const { scrollTop } = e.target  as HTMLTextAreaElement;;
+    const { scrollTop } = e.target as HTMLTextAreaElement;
     if (codeDisplayRef.current) {
       codeDisplayRef.current.scrollTop = scrollTop;
     }
   };
 
-const blinker = () => {
-  const lines = code.split('\n');
-  const lineNumber = lines.length;
-  const lastLineLength = lines[lineNumber - 1].length;
-  return {
-    top: (lineNumber - 1) * 1.5 + "em",
-    left: lastLineLength % 60 + "ch" 
-  };
-};
+  const wrapText = (wrapper: any) => {
+    const textarea = document.querySelector(
+      ".code-input"
+    ) as HTMLTextAreaElement;
+    if (!textarea) return;
 
-  
+    const start = textarea.selectionStart;
+    const end = textarea.selectionEnd;
+    const selectedText = textarea.value.slice(start, end);
+
+    if (start !== end) {
+      const newText = `${textarea.value.slice(
+        0,
+        start
+      )}${wrapper}${selectedText}${wrapper}${textarea.value.slice(end)}`;
+      setCode(newText);
+
+      setTimeout(() => {
+        textarea.selectionStart = start + wrapper.length;
+        textarea.selectionEnd = end + wrapper.length;
+      }, 0);
+    }
+  };
+
+  const makeBold = () => wrapText("**");
+
+  const convertMarkdownToHtml = (markdownText: any) => {
+    let htmlText = markdownText;
+    htmlText = htmlText.replace(/\*\*(.*?)\*\*/g, "<strong>$1</strong>");
+    return htmlText;
+  };
 
   return (
     <>
-    <Navbar />
+      <Navbar />
+      <button onClick={makeBold} className="font-bold text-white">
+        Bold
+      </button>
       <div className="flex ">
         <pre className="text-center select-none border-r border-gray-700 text-gray-500 font-mono h-[100vh] px-2">
           {getLineNumbers()}
@@ -55,34 +78,28 @@ const blinker = () => {
             spellCheck="false"
             className="code-input absolute top-0 left-0 w-full h-full z-10 bg-transparent resize-none outline-none font-mono text-white opacity-0"
           />
+
           <pre
             ref={codeDisplayRef}
             className="code-display absolute top-0 left-0 w-full h-full z-0 overflow-hidden font-mono bg-black text-white flex"
           >
-            <code className='language-javascript'>
-              {code}
-            </code>
-            <div className="absolute h-[1.4rem] bg-white w-[4px] blinker" style={{ top: blinker().top, left: blinker().left }}></div>
+            <code>{code}</code>
 
+            <div
+              dangerouslySetInnerHTML={{ __html: convertMarkdownToHtml(code) }}
+            />
           </pre>
         </div>
         <style jsx>{`
-          .line-numbers, .code-input, .code-display {
-            line-height: 1.5; 
-            font-size: 16px; 
+          .line-numbers,
+          .code-input,
+          .code-display {
+            line-height: 1.5;
+            font-size: 16px;
             margin: 0;
             padding: 0;
             box-sizing: border-box;
             background: black;
-          }
-
-          .blinker {
-            animation: blink-animation 1s steps(5, start) infinite;
-          }
-          @keyframes blink-animation {
-            to {
-              visibility: hidden;
-            }
           }
         `}</style>
       </div>
