1e6e90d461d31368156cdd7ceda15e5e5891e0c9
Oluwakayode Michael <104535663+oluwakayodemike@users.noreply.github.com>
2023-12-09 13:16:22 +0000

    no more logging for name, text box for file creation

--- components/NavBar/Navbar.tsx
+++ components/NavBar/Navbar.tsx
@@ -1,18 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 const Navbar: React.FC = () => {
   const [files, setFiles] = useState<string[]>([]);
+  const [isCreatingFile, setIsCreatingFile] = useState(false);
+  const [newFileName, setNewFileName] = useState('');
+  const inputRef = useRef<HTMLInputElement | null>(null);
 
   const handleNewFile = () => {
-    const fileName = prompt('Enter the name of the new file:');
-    if (fileName) {
-      setFiles((prevFiles) => [...prevFiles, fileName]);
+    setIsCreatingFile(true);
+  };
+
+  const handleFileNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewFileName(e.target.value);
+  };
+
+  const handleFileNameSubmit = () => {
+    if (newFileName.trim() !== '') {
+      setFiles((prevFiles) => [...prevFiles, newFileName]);
+      setNewFileName('');
+      setIsCreatingFile(false);
+    } else {
+      // no file name, close box
+      setIsCreatingFile(false);
+    }
+  };
+
+  const handleBlur = () => {
+    handleFileNameSubmit();
+  };
+
+  // enter to submit
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      handleFileNameSubmit();
     }
   };
 
   return (
     <div>
-      <button onClick={handleNewFile} className="btn btn-light" style={{ color: 'white' }}>New File</button>
+      <button onClick={handleNewFile} className="btn btn-light" style={{ color: 'white' }}>
+        New File
+      </button>
+      {isCreatingFile && (
+        <input
+          ref={inputRef}
+          type="text"
+          value={newFileName}
+          onChange={handleFileNameChange}
+          onBlur={handleBlur}
+          onKeyDown={handleKeyDown}
+          placeholder="Enter file name"
+          className="mx-2"
+          autoFocus
+        />
+      )}
       <div className="text-white">
         {files.map((file, index) => (
           <span key={index} className="mx-1">
